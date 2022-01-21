c0270232e82930d414d617c344b9e77d51237712
przemek-j <przemyslawjadas@gmail.com>
2022-01-21 13:00:41 +0100

    putting users into database

---
+++ GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/AddUserControler.java
@@ -0,0 +1,31 @@
+package com.pjada.GeneratorPdf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AddUserControler {
+    private UserRepo userRepo;
+    @Autowired
+    public AddUserControler(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public String addUser(
+            @RequestParam("name") String name,
+            @RequestParam("password") String pass,
+                Model model)
+        throws Exception{
+        User user = new User(name,pass);
+        userRepo.save(user);
+        System.out.println(user);
+        model.addAttribute("user",user);
+        return "index";
+    }
+
+}
