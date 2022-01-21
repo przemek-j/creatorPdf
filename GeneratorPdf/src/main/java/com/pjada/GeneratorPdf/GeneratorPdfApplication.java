a86638c195e03332176c541f84c2ef4c9d8015c1
przemek-j <przemyslawjadas@gmail.com>
2022-01-21 10:41:31 +0100

    Create maven project

---
+++ GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/GeneratorPdfApplication.java
@@ -0,0 +1,13 @@
+package com.pjada.GeneratorPdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorPdfApplication.class, args);
+	}
+
+}
