79195caf20bab88eb419e55d84bc23a243e1b0ab
przemek-j <przemyslawjadas@gmail.com>
2022-01-21 13:02:03 +0100

    putting users into database

--- GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/User.java
+++ GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/User.java
@@ -32,4 +32,12 @@ public class User {
         this.password = password;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
