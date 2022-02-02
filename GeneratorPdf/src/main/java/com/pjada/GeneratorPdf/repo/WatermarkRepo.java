1403f97321dd59e282fab7d35ee8aee04128a3f6
przemek-j <przemyslawjadas@gmail.com>
2022-02-02 19:51:41 +0100

    working on watermark view, status: bad encoding

--- GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/repo/WatermarkRepo.java
+++ GeneratorPdf/src/main/java/com/pjada/GeneratorPdf/repo/WatermarkRepo.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
+import java.util.List;
 
 @Repository
 public interface WatermarkRepo extends JpaRepository<Watermark, Integer> {
     HashSet<Watermark> findByUser(User user);
+
+    List<Watermark> findAllByName(String watermark);
+    List<Watermark> findAllByUser_Id(Integer id);
 }
